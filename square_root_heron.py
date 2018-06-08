761dd9f3f6e33e4724f512d5c8473934d152bf48
Arun@Home <arunisnowhere@gmail.com>
2018-06-08 23:16:49 +0300

    add precision that can also be entered by user

--- square_root_heron.py
+++ square_root_heron.py
@@ -3,7 +3,7 @@
 Arun Narayanan
 Started: May 26, 2018
 Ended: May 26, 2018 
-Last Modified: June 5, 2018 
+Last Modified: June 8, 2018 
 """
 
 # Heron's method for finding the approximate square root of a non-negative real number
@@ -15,42 +15,52 @@ Last Modified: June 5, 2018
 
 import math
 
-def square_root_heron(x, precision = 12, iterations = False):
+def square_root_heron(x, precision = 2, iterations = False):
     '''
     Return square root for any nonnegative number
     Uses Heron's method
-    Optionally returns the number of iterations to determine the square root
-    (x, boolean) -> (sqrt(x), nIterations)
-    (float, bool) -> (float, int)
+    Required precision can be input; default setting = 2    
+    Optionally returns the number of iterations used to determine the square root
+    (x, precision, iterations) -> (sqrt(x), -sqrt(x), nIterations)
+    (float, int, bool) -> (float, float, int)
     Examples
     >>> print(square_root_heron(1225))
-    35.0
-    >>> print(square_root_heron(1225,True))
-    (35.0, 8)
-    >>> print(square_root_heron(1225,1)
-    Error: optional parameter must be True or False
-    >>> print(square_root_heron(1225,'True')
-    Error: optional parameter must be True or False
-    '''    
+    (35.0, -35.0)
+    >>> print(square_root_heron(1000))
+    (31.62, -31.62)
+    >>> print(square_root_heron(1000,5))
+    (31.62278, -31.62278)
+    >>> print(square_root_heron(1000,'5'))
+    Error: precision must be an integer
+    >>> print(square_root_heron(1000,5.0))
+    Error: precision must be an integer
+    >>> print(square_root_heron(1000,True))
+    Error: precision must be an integer
+    >>> print(square_root_heron(1000,2,'True'))
+    Error: request for number of iterations must be boolean True or False
+    '''
+    # Error check for precision
+    if not (type(precision) == int):
+        return ("Error: precision must be an integer")
+    # Error check for iterations
     if not (type(iterations) == type(True)):
-        return ("Error: optional parameter must be True or False")
+        return ("Error: request for number of iterations must be boolean True or False")
+    # Error check for x (must be >= 0)
     if x < 0:
         return ("Error: please enter nonnegative number only")
     if x == 0:
         return 0
     y = 2**(math.frexp(10)[1]/2) # guess the value of y, suggested in https://math.stackexchange.com/questions/521981/picking-an-appropriate-guess-for-herons-method-and-the-fast-reciprocal-method    
-    epsilon = 1/(10**precision) # set error
-    print (epsilon)
+    epsilon = 1/(10**precision) # set error    
     iter = 0
-    while (abs(x-y**2)>=epsilon):        
+    while (abs(x-y**2)>epsilon):        
         iter += 1
         y = (y+x/y)/2
+    # the while test tests precision of y**2 not y itself, so this rounding step is required
+    # then, why check against precision in the while test?
+    # to decrease the number of iterations required. lower reqd. precision => fewer iterations
+    y = round(y,precision)
     if iterations:
-        return y -y, iter
+        return y, -y, iter
     else:
         return y, -y
-    
-
-# TO DO
-# Let user enter required precision.
-# Add comments
